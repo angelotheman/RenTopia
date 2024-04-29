ff5519f143dd569018cea67db92e31f363a97c29
Kwabena Twumasi <ktwum721@gmail.com>
2024-04-29 10:40:15 +0000

    Updated files for project

--- app/main.py
+++ app/main.py
@@ -4,19 +4,22 @@ This is the entry point of the entire application
 """
 from fastapi import FastAPI
 from routers import user, apartment, booking, review
-#from database import engine
-#from models.base_model import Base
+# from database import engine
+# from models.base_model import Base
 
 
 app = FastAPI()
 
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 # Include routers
 app.include_router(user.router)
-#app.include_router(apartment.router)
-#app.include_router(booking.router)
-#app.include_router(review.router)
-
+# app.include_router(apartment.router)
+# app.include_router(booking.router)
+# app.include_router(review.router)
 
 # Create tables
-#Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
